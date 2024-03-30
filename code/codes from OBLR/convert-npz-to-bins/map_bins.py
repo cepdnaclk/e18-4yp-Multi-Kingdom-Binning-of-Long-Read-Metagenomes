69824ba71d3829032b97d464733a7c1664d4d06c
NethmiRanasinghe <e18282@eng.pdn.ac.lk>
2024-03-30 08:16:40 +0530

    add new evaluation script

--- code/codes from OBLR/convert-npz-to-bins/map_bins.py
+++ code/codes from OBLR/convert-npz-to-bins/map_bins.py
@@ -1,25 +1,26 @@
 import numpy as np
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage: python script.py classes.npz read_ids")
+if len(sys.argv) != 2:
+    print("Usage: python script.py classes.npz")
     sys.exit(1)
 
 classes_file = sys.argv[1]
-read_ids_file = sys.argv[2]
+# read_ids_file = sys.argv[2]
 
 # Unzipping and reading the classes.npz file
 with np.load(classes_file) as data:
     class_array = data['classes']
+    read_ids = data['classified']
 
 # Reading read_ids file and extracting read names
-with open(read_ids_file, 'r') as file:
-    read_names = [line.split()[0][1:] for line in file if line.strip()]
+# with open(read_ids_file, 'r') as file:
+#     read_names = [line.split()[0][1:] for line in file if line.strip()]
 
 # Mapping values from class_array to read_names
-mapped_values = dict(zip(read_names, class_array))
+mapped_values = dict(zip(read_ids, class_array))
 
 # Writing mapped values to bins.txt
-with open('bins.txt', 'w') as output_file:
-    for read_name in read_names:
-        output_file.write(f"{read_name} {mapped_values.get(read_name)}\n")
+with open('bins.tsv', 'w') as output_file:
+    for read_number in read_ids:
+        output_file.write(f"read_{read_number}\t{mapped_values.get(read_number)}\n")
